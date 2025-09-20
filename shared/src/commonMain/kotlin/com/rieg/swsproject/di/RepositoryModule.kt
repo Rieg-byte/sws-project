1b1ae059fc7bb17d2d5dbcfd6ce64a77458ca60e
Lineked <lineked@yandex.ru>
2025-09-20 12:13:09 +0300

    Добавлен WorkshopItemRepositoryImpl

--- shared/src/commonMain/kotlin/com/rieg/swsproject/di/RepositoryModule.kt
+++ shared/src/commonMain/kotlin/com/rieg/swsproject/di/RepositoryModule.kt
@@ -1,11 +1,14 @@
 package com.rieg.swsproject.di
 
 import com.rieg.swsproject.data.repository.SteamApiKeyRepositoryImpl
+import com.rieg.swsproject.data.repository.WorkshopItemRepositoryImpl
 import com.rieg.swsproject.domain.repository.SteamApiKeyRepository
+import com.rieg.swsproject.domain.repository.WorkshopItemRepository
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val repositoryModule = module {
     singleOf(::SteamApiKeyRepositoryImpl).bind<SteamApiKeyRepository>()
+    singleOf(::WorkshopItemRepositoryImpl).bind<WorkshopItemRepository>()
 }
